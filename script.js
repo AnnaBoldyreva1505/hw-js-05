7a5cbfcf401223dcc4cf9912ddfd2cd15a87c930
Anna Boldyreva <104367856+AnnaBoldyreva1505@users.noreply.github.com>
2022-09-01 12:22:58 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const bookShelf = {
+    authors: ["Bernard Cornwell", "Robert Sheckley"],
+    getAuthors() {
+      return this.authors;
+    },
+    addAuthor(authorName) {
+      this.authors.push(authorName);
+    },
+  };
+  
+  console.log(bookShelf.getAuthors()); // ["Bernard Cornwell", "Robert Sheckley"]
+  bookShelf.addAuthor("Tanith Lee");
+  console.log(bookShelf.getAuthors()); // ["Bernard Cornwell", "Robert Sheckley", "Tanith Lee"]
